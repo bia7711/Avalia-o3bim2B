255e034db18c3262061a8546b7bba76e9d535e65
Beatriz <00001092119681sp@al.educacao.sp.gov.br>
2024-10-01 21:12:41 -0300

    olá mundo

---
+++ main.py
@@ -0,0 +1,15 @@
+login= str(input("Digite seu login:"))
+senha= int(input("Digite sua senha:"))
+
+if senha <=8:
+  print("senha inválida,digite novamente")
+elif senha >=8:
+ print(isapha('senha')
+ print(isdigit('senha') 
+      
+
+   
+  
+
+  
+  
